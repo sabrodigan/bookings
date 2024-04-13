4678d0d462719b59d9d87a4fe78c2129a6826dc8
Stephen Brodigan <sabrodigan@gmail.com>
2024-04-13 17:11:29 +0100

    setting up project bookings

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -29,19 +29,23 @@ func NewHandlers(r *Repository) {
 
 // Home is the handler for the home page
 func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+	remoteIP := r.RemoteAddr
+	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
 	render.TempRendered(w, "home.page.tmpl", &models.TemplateData{})
 
-}
-
-// About is the handler for the about page
+} // About is the handler for the about page
 func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 	// perform some logic
 	stringMap := make(map[string]string)
 	stringMap["test"] = "Hello, again, this is dynamic content, which is sent to the template using the EXECUTE command and including the data in the TemplateData struct.  Once the template has the data, we can show a search page that allows for freetex search of the boxes database."
+
+	remoteIP := m.App.Session.GetString(r.Context(), "remote_ip")
+	stringMap["remote_ip"] = remoteIP
 
 	// send data to the template
 	render.TempRendered(w, "about.page.tmpl", &models.TemplateData{
 		StringMap: stringMap,
 		Flash:     "This is a flash message",
 	})
+
 }
